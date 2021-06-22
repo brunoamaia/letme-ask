ad2a6182bf1a992a1c1be13b8bb3be245b9acf3f
brunoamaia <brunoamaia94@gmail.com>
2021-06-22 15:18:41 -0300

    pagina new room e autenticação com o firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 import './styles/global.scss';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path='/' exact component={Home} />
+      <Route path='/rooms/new' component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,27 @@
+import { useHistory } from 'react-router-dom'
+
+import { Button } from '../components/Button'
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
-import {Button} from '../components/Button'
 
 import '../styles/auth.scss'
 
 export function Home() {
+  const history = useHistory()
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+    })
+
+   // history.push('./rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +33,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Lodo do Google" />
             Crie sua sala com o Google
           </button>
@@ -25,12 +41,11 @@ export function Home() {
           <div className="separator">ou entre em uma sala</div>
 
           <form action="">
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder="Digite o código da sala"
             />
 
-    
             <Button type="submit">
               Entrar na sala
             </Button>
